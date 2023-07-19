f7e652f33d10ad52cbae6dcbde5031513d086074
favioamarillapy <53306199+favioamarillapy@users.noreply.github.com>
2023-07-19 08:31:27 -0400

    Created alert screen

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,14 +3,51 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({super.key});
 
+  void displayAlert(BuildContext context) {
+    showDialog(
+      barrierDismissible: true,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Alert title"),
+          content: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FlutterLogo(size: 100),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: const Text("OK"),
+              onPressed: () => Navigator.pop(context),
+            ),
+            TextButton(
+              child: const Text("Cancel"),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Alert Screen"),
       ),
-      body: const Center(
-        child: Text("Alert Screen"),
+      body: Center(
+        child: Center(
+          child: ElevatedButton(
+            child: const Text("Show alert"),
+            onPressed: () => displayAlert(context),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.close),
+        onPressed: () => Navigator.pop(context),
       ),
     );
   }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,17 +4,24 @@ class AppTheme {
   static const Color primaryColor = Colors.indigo;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    primaryColor: primaryColor,
-    appBarTheme: const AppBarTheme(
-      color: primaryColor,
-      elevation: 0,
-    ),
-    textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(
-        foregroundColor: primaryColor,
+      primaryColor: primaryColor,
+      appBarTheme: const AppBarTheme(
+        color: primaryColor,
+        elevation: 0,
       ),
-    ),
-  );
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          foregroundColor: primaryColor,
+        ),
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        backgroundColor: primaryColor,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: TextButton.styleFrom(
+          backgroundColor: primaryColor,
+        ),
+      ));
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
     primaryColor: primaryColor,
